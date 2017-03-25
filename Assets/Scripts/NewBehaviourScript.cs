8ed0bbc7e8094ac9756970cd396ad253a3510c9f
Cvetelin333 <ceci77777@mail.bg>
2017-03-25 22:50:07 +0200

    Add files via upload
    Fixed NewBehaviorScript

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,34 +2,33 @@
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private Rigidbody2D rb;
     private int moveDirection;
-    public float acceleration = 75f;
-    private bool isActive = false;
+    public float acceleration = 5f;
+    bool isActive;
 
     void Start()
     {
-        this.rb = this.GetComponent<Rigidbody2D>();
+
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if(isActive)
+        if (isActive == true)
         {
-            if(moveDirection == 1)
+            if (moveDirection == 1)
             {
-                this.rb.AddForce(new Vector3(acceleration, 0f));
+                transform.Translate(new Vector3(acceleration, 0f, 0f));
             }
             else
             {
-
+                transform.Translate(new Vector3(-acceleration, 0f, 0f));
             }
         }
+
     }
 
-    public void ObjectMove(int direction)
+    public void OnPointerDown(int direction)
     {
-        isActive = true;
         if (direction == 1)
         {
             moveDirection = 1;
@@ -38,5 +37,11 @@ public class NewBehaviourScript : MonoBehaviour
         {
             moveDirection = -1;
         }
+        isActive = true;
+    }
+
+    public void OnPointerUp()
+    {
+        isActive = false;
     }
 }
