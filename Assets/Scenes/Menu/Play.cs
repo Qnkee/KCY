b9f1ed173a93b1278ed5886b9c237d107fcca5f1
Cvetelin333 <ceci77777@mail.bg>
2017-03-26 11:27:03 +0300

    Adde whole new project

---
+++ Assets/Scenes/Menu/Play.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void LoadScene(string stagename)
+    {
+        SceneManager.LoadScene(stagename);
+    }
+}
--- Assets/Scripts/trigger.cs
+++ Assets/Scripts/trigger.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class trigger : MonoBehaviour {
-    private float Ypos = -1.58f;
-    public GameObject koce;
+public class trigger : MonoBehaviour
+{
+    private float Ypos;
+    public GameObject Rainbow;
     private readonly object other;
     private bool colis = false;
+
     void FixedUpdate()
     {
-        if (colis==true)
+        if (colis == true)
         {
-            Vector3 FillPos = new Vector3(2.8f, Ypos, 0.0f);
-            koce.transform.position = FillPos;
-            Ypos = Ypos + 0.1f *Time.deltaTime;
-            if(Ypos> -0.3173597f) { colis=false; }
-           
-            //Pottentioal GameOver implamentation
+            Ypos = 0.1f * Time.deltaTime;
+            this.RiseUp(Ypos, colis, Rainbow);
         }
     }
     private void OnTriggerStay2D(Collider2D col)
@@ -26,10 +22,24 @@ public class trigger : MonoBehaviour {
         {
 
             colis = true;
-         
+
         }
 
-    
+
     }
     private void OnTriggerExit2D(Collider2D col) { colis = false; }
+
+    public void RiseUp(float Ypos, bool colis, GameObject Rainbow)
+    {
+        if (Rainbow.transform.position.y > 0.12f)
+        {
+            colis = false;
+            //End Game
+        }
+        else
+        {
+            Vector3 FillPos = new Vector3(0f, Ypos, 0.0f);
+            Rainbow.transform.position += FillPos;
+        }
+    }
 }
